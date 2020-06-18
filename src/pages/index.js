aaf6d51a1d69c48c110fd14aec2b827bbc70923d
stevenjchang <stevenjchang@gmail.com>
2020-06-17 18:55:09 -0700

    style: add logo

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import Logo from '../images/GTA.png'
 
 import { FaBeer } from "react-icons/fa"
 
@@ -6,14 +7,14 @@ const IndexPage = () => {
   return (
     <div className="font-sans">
       <nav className="h-16 sticky top-0 z-10 bg-white flex items-center">
-        <div className="container relative flex items-center py-4 bg-gray-100 w-5/6 lg:w-3/4">
-          <a href="" className="left-0 absolute ">
-            logo
+        <div className="container flex justify-between items-center bg-gray-10 w-5/6 lg:w-3/4">
+          <a href="" className="">
+            <img src={Logo} alt="" className="mb-0"/>
           </a>
-          <div className="css-nav-right right-0 absolute">
+          <div className="css-nav-right">
             <a href="" className="custom-nav-links">Features</a>
-            <a href="">Pricing</a>
-            <a href="">Contact Us</a>
+            <a href="" className="custom-nav-links">Pricing</a>
+            <a href="" className="custom-nav-links">Contact Us</a>
           </div>
         </div>
       </nav>
