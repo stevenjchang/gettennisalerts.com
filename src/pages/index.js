fb6b16eea9011d2678ac9098a6761539afbbe972
stevenjchang <stevenjchang@gmail.com>
2020-06-18 16:47:43 -0700

    style: add hero section

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import React from "react"
-import Logo from '../images/GTA.png'
+import LogoImg from "../images/GTA.png"
+import HeroImg from "../images/undraw_grand_slam_0q5r 1.png"
 
-import { FaBeer } from "react-icons/fa"
+// import { FaBeer } from "react-icons/fa"
 
 const IndexPage = () => {
   return (
@@ -9,90 +10,49 @@ const IndexPage = () => {
       <nav className="h-16 sticky top-0 z-10 bg-white flex items-center">
         <div className="container flex justify-between items-center bg-gray-10 w-5/6 lg:w-3/4">
           <a href="" className="">
-            <img src={Logo} alt="" className="mb-0"/>
+            <img src={LogoImg} alt="" className="mb-0" />
           </a>
           <div className="css-nav-right">
-            <a href="" className="custom-nav-links">Features</a>
-            <a href="" className="custom-nav-links">Pricing</a>
-            <a href="" className="custom-nav-links">Contact Us</a>
+            <a href="" className="custom-nav-links">
+              Features
+            </a>
+            <a href="" className="custom-nav-links">
+              Pricing
+            </a>
+            <a href="" className="custom-nav-links">
+              Contact Us
+            </a>
           </div>
         </div>
       </nav>
       <div className="css-shadow-div sticky w-full"></div>
       <div className="main-container relative">
         <div className="cover h-1 absolute w-full bg-white"></div>
-        <div>extra <a href="">a tag</a></div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
-        <div>extra</div>
+        <div className="container">
+          <section className="container custom-section-1 flex h-screen w-3/4">
+            <div className="custom-left w-1/2 flex flex-col justify-center items-start">
+              <h1 className="text-5xl font-bold mb-8">
+                Never miss another tennis match
+              </h1>
+              <h3 className="text-gray-600 tracking-wide w-2/3">
+                Follow your favorite tennis stars and be notified before each
+                match starts. We will email you 1 day and 1 hour before each
+                match
+              </h3>
+              <div className="mt-10">
+                <button className="custom-cta-button custom-button-1 bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 hover:border-transparent rounded">
+                  find out more
+                </button>
+                <button className="custom-cta-button custom-button-2 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded">
+                  sign up now
+                </button>
+              </div>
+            </div>
+            <div className="custom-right w-1/2 flex justify-center items-center">
+              <img src={HeroImg} alt="" />
+            </div>
+          </section>
+        </div>
       </div>
     </div>
   )
