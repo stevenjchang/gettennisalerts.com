74299fcc4e537a7ad84ac9a075e6842405ffdf47
stevenjchang <stevenjchang@gmail.com>
2020-06-17 17:18:46 -0700

    npm i react-icons

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,26 @@
-import React from "react";
+import React from "react"
+
+import { FaBeer } from "react-icons/fa"
 
 const IndexPage = () => {
   return (
-    <div>
-      <nav className="h-16 sticky top-0 z-10 bg-white">
-        <a href="">logo</a>
-        <a href="">Features</a>
-        <a href="">Pricing</a>
-        <a href="">Contact Us</a>
-      </nav>
-      <div className="shadow-div sticky w-full"></div>
-      <div className="main-container relative">
-        <div className="cover h-1 absolute w-full bg-white">
+    <div className="font-sans">
+      <h3> Lets go for a <FaBeer />? </h3>
+      <nav className="h-16 sticky top-0 z-10 bg-white flex items-center">
+        <div className="container relative flex items-center py-4">
+          <a href="" className="left-0 absolute ">
+            logo
+          </a>
+          <div className="css-nav-right right-0 absolute">
+            <a href="">Features</a>
+            <a href="">Pricing</a>
+            <a href="">Contact Us</a>
+          </div>
         </div>
+      </nav>
+      <div className="css-shadow-div sticky w-full"></div>
+      <div className="main-container relative">
+        <div className="cover h-1 absolute w-full bg-white"></div>
         <div>extra</div>
         <div>extra</div>
         <div>extra</div>
@@ -90,4 +98,4 @@ const IndexPage = () => {
   )
 }
 
-export default IndexPage;
+export default IndexPage
