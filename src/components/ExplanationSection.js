73829625a8f2d3f89737b0218b53a39f4b82bc99
stevenjchang <stevenjchang@gmail.com>
2020-06-20 23:44:29 -0700

    text: update paragraphs

--- src/components/ExplanationSection.js
+++ src/components/ExplanationSection.js
@@ -4,18 +4,21 @@ import Img2 from "../images/undraw_email_campaign_qa8y.png"
 import Img3 from "../images/undraw_mail_2_tqip.png"
 import Img4 from "../images/undraw_mobile_inbox_3h46.png"
 import Img5 from "../images/image_followers.png"
+import Img6 from "../images/undraw_online_articles_79ff.png"
 
-const imgArray = [Img5, Img1, Img3, Img2]
+const imgArray = [Img5, Img3, Img6, Img2]
 const heading = [
   "Follow Players",
-  "Set Alerts",
+  // "Set Alerts",
   "Email Notifications",
+  "View Scores",
   "And More",
 ]
 const para = [
   "",
   "",
   "",
+  "Enjoy a clean and simplified view, only browse through the score of the players you follow. This feature is in beta.",
   "Currently in beta, there are more features to come. Sign up to be notified of new features, or take our survey",
 ]
 
@@ -23,25 +26,32 @@ const ExplanationSection = () => {
   return (
     <>
       <div className="bg">
-        <div className="container flex flex-col my-20 justify-center w-5/6">
+        <div className="container flex flex-col my-20 w-9/12">
           {imgArray.map((img, idx) => {
-            let cn = idx % 2 === 0 ? "flex m-3" : "flex m-3 flex-row-reverse"
+            let cn =
+              idx % 2 === 0
+                ? "flex my-3 justify-around"
+                : "flex my-3 justify-around flex-row-reverse"
             return (
               <div className={cn}>
                 <div className="w-2/5 custom-flex-center flex-row">
                   <img src={img} alt="select player" className="" />
                 </div>
-                <div className="w-3/5 flex justify-center flex-col items-center">
+                <div className="w-1/2 flex justify-center flex-col items-center">
                   <h1 className="font-bold uppercase">{heading[idx]}</h1>
                   <hr className="mt-5 mb-5" />
-                  <p className="custom-text-gray w-3/4 ">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Cras tincidunt orci ut suscipit pretium. Class aptent taciti
-                    sociosqu ad litora torquent per conubia nostra, per inceptos
-                    himenaeos. Lorem ipsum dolor sit amet, consectetur
-                    adipiscing elit. Cras tincidunt orci ut suscipit pretium
-                    {para[idx]}
-                  </p>
+                  {para[idx] ? (
+                    <p className="custom-text-gray ">{para[idx]}</p>
+                  ) : (
+                    <p className="custom-text-gray ">
+                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                      Cras tincidunt orci ut suscipit pretium. Class aptent
+                      taciti sociosqu ad litora torquent per conubia nostra, per
+                      inceptos himenaeos. Lorem ipsum dolor sit amet,
+                      consectetur adipiscing elit. Cras tincidunt orci ut
+                      suscipit pretium
+                    </p>
+                  )}
                 </div>
               </div>
             )
