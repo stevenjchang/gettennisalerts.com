292158f87f754f6c48c2be67878b53b8fe8c676e
stevenjchang <stevenjchang@gmail.com>
2020-06-22 17:36:14 -0700

    style: fix horizontal scroll (remove container p)

--- src/components/EmailSignUp.js
+++ src/components/EmailSignUp.js
@@ -4,7 +4,7 @@ const EmailSignUp = () => {
   return (
     <div className="custom-background-color bg-gray-400">
       <section className="container text-center text-white h-48 custom-flex-center">
-        <div className="form-container custom-flex-center flex-col  px-16 py-8 rounded-md">
+        <div className="form-container custom-flex-center flex-col py-8 rounded-md">
           <div className="mb-6 text-gray-800">
             something something something something something something
           </div>
